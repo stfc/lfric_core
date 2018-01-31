ticket:979 for Tom Melvin: Fix vorticity computation.
git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@12779 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

gungho/rose-stem/app/plot/bin/robert_plot.py
@@ -78,6 +78,10 @@ def make_figure(plotpath, nx, ny, field, component, timestep):
   background = 0.0
   if field == 'theta':
     background = 303.05
+    cc = np.linspace(-0.1, 0.6, 15)
+  else:
+    cc = np.linspace(np.amin(zi), np.amax(zi), 11)
+
   for i in range(nx):
     dz[i,:] = zi[0,i,:] - background 
 
@@ -92,7 +96,7 @@ def make_figure(plotpath, nx, ny, field, component, timestep):
   plt.title('max: %2.4e, min: %2.4e'%(np.max(dz),np.min(dz)))
   plt.colorbar(cf,  cmap=c_map)
 
-  out_file_name = plotpath + "/" + 'robert' + "_" + timestep +  ".png"
+  out_file_name = plotpath + "/" + 'robert_' + field + "_" + timestep +  ".png"
   slice_fig.savefig(out_file_name , bbox_inches='tight')
 
 

gungho/rose-stem/app/plot/bin/straka_plot_x.py
@@ -75,12 +75,20 @@ def make_figure(plotpath, nx, ny, field, component, timestep):
   y_i, x_i = np.meshgrid(z2d, x2d) 
 
   dz = np.zeros([nx,len(levels)])
+  back = 0.0
+  if field == 'theta':
+    back = 300.0
+    cc = np.linspace(-16, -1, 16)
+  elif field == 'w3projection_xi2':
+    cc =  np.linspace(-0.06,0.06,11)
+  else:
+    cc = np.linspace(np.amin(zi),np.amax(zi), 11)
+
   for i in range(nx):
-    dz[i,:] = zi[0,i,:] - 300.0
+    dz[i,:] = zi[0,i,:] - back
 
   matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
   c_map = cm.summer
-  cc = np.linspace(-16, -1, 16)
   cf = plt.contourf(x_i * r2d, y_i * r2d, np.round(dz,10), cc, cmap=c_map)
   cl = plt.contour(x_i * r2d, y_i * r2d, np.round(dz,10), cc, linewidths=1.0,colors='k', linestyle="", extend='min')
   plt.axis([0, 16, 0, 5])
@@ -89,7 +97,7 @@ def make_figure(plotpath, nx, ny, field, component, timestep):
   plt.title('max: %2.4e, min: %2.4e'%(np.max(dz),np.min(dz)))
   plt.colorbar(cf,  cmap=c_map)
 
-  out_file_name = plotpath + "/" + 'straka_x' + "_" + timestep +  ".png"
+  out_file_name = plotpath + "/" + 'straka_x_' + field + "_" + timestep +  ".png"
   slice_fig.savefig(out_file_name , bbox_inches='tight')
 
 