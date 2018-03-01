3af79908a58c3c69925e7ff9ab70557faf4ae529
ivakavcic <enquiries@metoffice.gov.uk>
2018-03-01 11:07:06 +0000

    ticket:1378 for Matthew Hambley: Replace visualisation Makefile with proper tool - LFRic trunk.
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@13093 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- gungho/documentation/visualisation/tools/doctools/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2.7
-# -*- coding: utf-8 -*-
-##############################################################################
-# Copyright (c) 2017,  Met Office, on behalf of HMSO and Queen's Printer
-# For further details please refer to the file LICENCE.original which you
-# should have received as part of this distribution.
-##############################################################################
