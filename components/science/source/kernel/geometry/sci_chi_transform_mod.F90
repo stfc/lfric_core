0c61331a96d0c57ba15723ab190e8500e2ffd1c5
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2026-03-23 16:16:10 +0000

    isnan isn't valid

--- components/science/source/kernel/geometry/sci_chi_transform_mod.F90
+++ components/science/source/kernel/geometry/sci_chi_transform_mod.F90
@@ -172,7 +172,7 @@ subroutine init_chi_transforms( geometry, topology, &
     end if
     ! Weird lfric_atm bug where null_islane(2) was NaN, this is a temporary fix
     ! to this.
-    if( isnan(null_island(2))) then
+    if( null_island(2) /= null_island(2)) then
         null_island(2) = 0.0_r_def
     end if
     if ( abs(null_island(1) - rmdi) < EPS                                      &
